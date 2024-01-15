ea7d44af97aadef0b0ef73bdafdf78b01c2ca444
gauravgawali7055 <gauravgawali7055@gmail.com>
2024-01-15 09:04:53 +0530

    my first java program SUCCESFUL

---
+++ first1.java
@@ -0,0 +1,12 @@
+class first1
+{
+    private void demo()
+    {
+        system.out.println("hello java ")
+    }
+    public static void main( String args[])
+    {
+     first1 f=new first();
+     f.demo
+    }
+}
